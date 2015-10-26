2a36365825141ac156d41319d606d01e0f2139b2
Willdorf <william.g.armstrong@gmail.com>
2015-10-26 13:19:54 -0400

    basic watch setup

--- src/blocky.c
+++ src/blocky.c
@@ -1,5 +1,18 @@
 #include <pebble.h>
 
+#define KEY_BLOCK_ONE_COLOR 0
+#define KEY_BLOCK_TWO_COLOR 1
+#define KEY_BLOCK_THREE_COLOR 2
+#define KEY_BLOCK_FOUR_COLOR 3
+#define KEY_BLOCK_FIVE_COLOR 4
+#define KEY_BLOCK_SIX_COLOR 5
+#define KEY_BLOCK_SEVEN_COLOR 6
+#define KEY_BLOCK_EIGHT_COLOR 7
+#define KEY_BLOCK_NINE_COLOR 8
+#define KEY_BLOCK_TEN_COLOR 9
+#define KEY_BLOCK_ELEVEN_COLOR 10
+#define KEY_BLOCK_TWELVE_COLOR 11
+
 static Window *window;
 static Layer *s_layer;
 static TextLayer *s_time_layer;
@@ -22,6 +35,19 @@ static GRect nine;
 static GRect ten;
 static GRect eleven;
 static GRect twelve;
+
+static GColor blockOneColor;
+static GColor blockTwoColor;
+static GColor blockThreeColor;
+static GColor blockFourColor;
+static GColor blockFiveColor;
+static GColor blockSixColor;
+static GColor blockSevenColor;
+static GColor blockEightColor;
+static GColor blockNineColor;
+static GColor blockTenColor;
+static GColor blockElevenColor;
+static GColor blockTwelveColor;
 
 static void update_time(struct tm *tick_time) {
 	s_hour = tick_time->tm_hour;
@@ -53,27 +79,81 @@ static void setup_blocks() {
 	ten = GRect(72, 116, 24, 24);
 	eleven = GRect(96, 116, 24, 24);
 	twelve = GRect(120, 92, 24, 24);
+
+	if (persist_read_int(KEY_BLOCK_ONE_COLOR)) {
+		blockOneColor = GColorFromHEX(persist_read_int(KEY_BLOCK_ONE_COLOR));
+	} else {
+		blockOneColor = GColorRed;
+	}
+
+	if (persist_read_int(KEY_BLOCK_TWO_COLOR)) {
+		blockTwoColor = GColorFromHEX(persist_read_int(KEY_BLOCK_TWO_COLOR));
+	} else {
+		blockTwoColor = GColorRed;
+	}
+
+	if (persist_read_int(KEY_BLOCK_THREE_COLOR)) {
+		blockThreeColor = GColorFromHEX(persist_read_int(KEY_BLOCK_THREE_COLOR));
+	} else {
+		blockThreeColor = GColorRed;
+	}
+
+	if (persist_read_int(KEY_BLOCK_FOUR_COLOR)) {
+		blockFourColor = GColorFromHEX(persist_read_int(KEY_BLOCK_FOUR_COLOR));
+	} else {
+		blockFourColor = GColorRed;
+	}
+
+	if (persist_read_int(KEY_BLOCK_FIVE_COLOR)) {
+		blockFiveColor = GColorFromHEX(persist_read_int(KEY_BLOCK_FIVE_COLOR));
+	} else {
+		blockFiveColor = GColorRed;
+	}
+
+	if (persist_read_int(KEY_BLOCK_SIX_COLOR)) {
+		blockSixColor = GColorFromHEX(persist_read_int(KEY_BLOCK_SIX_COLOR));
+	} else {
+		blockSixColor = GColorRed;
+	}
+
+	if (persist_read_int(KEY_BLOCK_SEVEN_COLOR)) {
+		blockSevenColor = GColorFromHEX(persist_read_int(KEY_BLOCK_SEVEN_COLOR));
+	} else {
+		blockSevenColor = GColorRed;
+	}
+
+	if (persist_read_int(KEY_BLOCK_EIGHT_COLOR)) {
+		blockEightColor = GColorFromHEX(persist_read_int(KEY_BLOCK_EIGHT_COLOR));
+	} else {
+		blockEightColor = GColorRed; 
+	}
+
+	if (persist_read_int(KEY_BLOCK_NINE_COLOR)) {
+		blockNineColor = GColorFromHEX(persist_read_int(KEY_BLOCK_NINE_COLOR));
+	} else {
+		blockNineColor = GColorRed; 
+	}
+
+	if (persist_read_int(KEY_BLOCK_TEN_COLOR)) {
+		blockTenColor = GColorFromHEX(persist_read_int(KEY_BLOCK_TEN_COLOR));
+	} else {
+		blockTenColor = GColorRed;
+	}
+
+	if (persist_read_int(KEY_BLOCK_ELEVEN_COLOR)) {
+		blockElevenColor = GColorFromHEX(persist_read_int(KEY_BLOCK_ELEVEN_COLOR));
+	} else {
+		blockElevenColor = GColorRed;
+	}
+
+	if (persist_read_int(KEY_BLOCK_TWELVE_COLOR)) {
+		blockTwelveColor = GColorFromHEX(persist_read_int(KEY_BLOCK_TWELVE_COLOR));
+	} else {
+		blockTwelveColor = GColorRed;
+	}
 }
 
 static void draw_watchface(Layer *layer, GContext *ctx) {
-	graphics_context_set_fill_color(ctx, gcolor_legible_over(background_color));
-
-	//draw the blocks
-	graphics_context_set_stroke_color(ctx, gcolor_legible_over(background_color));
-	graphics_context_set_stroke_width(ctx, 3);
-
-	graphics_draw_rect(ctx, one);
-	graphics_draw_rect(ctx, two);
-	graphics_draw_rect(ctx, three);
-	graphics_draw_rect(ctx, four);
-	graphics_draw_rect(ctx, five);
-	graphics_draw_rect(ctx, six);
-	graphics_draw_rect(ctx, seven);
-	graphics_draw_rect(ctx, eight);
-	graphics_draw_rect(ctx, nine);
-	graphics_draw_rect(ctx, ten);
-	graphics_draw_rect(ctx, eleven);
-	graphics_draw_rect(ctx, twelve);
 
 	uint8_t cur_hour = s_hour % 12;
 	if (cur_hour == 0) {
@@ -136,6 +216,65 @@ static void draw_watchface(Layer *layer, GContext *ctx) {
 	}
 
 	layer_set_frame((Layer *) s_time_layer, f);
+
+	switch (cur_hour) {
+		case 12:
+			graphics_context_set_fill_color(ctx, blockTwelveColor);
+			graphics_fill_rect(ctx, twelve, 0 , 0);
+		case 11:
+			graphics_context_set_fill_color(ctx, blockElevenColor);
+			graphics_fill_rect(ctx, eleven, 0 , 0);
+		case 10:
+			graphics_context_set_fill_color(ctx, blockTenColor);
+			graphics_fill_rect(ctx, ten, 0 , 0);
+		case 9:
+			graphics_context_set_fill_color(ctx, blockNineColor);
+			graphics_fill_rect(ctx, nine, 0 , 0);
+		case 8:
+			graphics_context_set_fill_color(ctx, blockEightColor);
+			graphics_fill_rect(ctx, eight, 0 , 0);
+		case 7:
+			graphics_context_set_fill_color(ctx, blockSevenColor);
+			graphics_fill_rect(ctx, seven, 0 , 0);
+		case 6:
+			graphics_context_set_fill_color(ctx, blockSixColor);
+			graphics_fill_rect(ctx, six, 0 , 0);
+		case 5:
+			graphics_context_set_fill_color(ctx, blockFiveColor);
+			graphics_fill_rect(ctx, five, 0 , 0);
+		case 4:
+			graphics_context_set_fill_color(ctx, blockFourColor);
+			graphics_fill_rect(ctx, four, 0 , 0);
+		case 3:
+			graphics_context_set_fill_color(ctx, blockThreeColor);
+			graphics_fill_rect(ctx, three, 0 , 0);
+		case 2:
+			graphics_context_set_fill_color(ctx, blockTwoColor);
+			graphics_fill_rect(ctx, two, 0, 0);
+		case 1:
+			graphics_context_set_fill_color(ctx, blockOneColor);
+			graphics_fill_rect(ctx, one, 0, 0);
+		default :
+			APP_LOG(APP_LOG_LEVEL_DEBUG, "invalid hour: %d", cur_hour);
+	}
+
+	//draw the blocks
+	graphics_context_set_stroke_color(ctx, gcolor_legible_over(background_color));
+	graphics_context_set_stroke_width(ctx, 3);
+
+	graphics_draw_rect(ctx, one);
+	graphics_draw_rect(ctx, two);
+	graphics_draw_rect(ctx, three);
+	graphics_draw_rect(ctx, four);
+	graphics_draw_rect(ctx, five);
+	graphics_draw_rect(ctx, six);
+	graphics_draw_rect(ctx, seven);
+	graphics_draw_rect(ctx, eight);
+	graphics_draw_rect(ctx, nine);
+	graphics_draw_rect(ctx, ten);
+	graphics_draw_rect(ctx, eleven);
+	graphics_draw_rect(ctx, twelve);
+
 }
 
 static void window_load(Window *window) {
